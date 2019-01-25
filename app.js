3cb88df18e046b3d028ccda2cd430b1fd24f6187
AnanaGame <gameanana@gmail.com>
2019-01-25 14:31:08 +0100

    Begin the solve method WIP

--- app.js
+++ app.js
@@ -15,22 +15,27 @@ const maze = [
   [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
 ]
 
-function isInRange(maze, x, y) {
+function isInMaze(maze, x, y) {
   return x >= 0 && y >= 0 && x < maze[0].length && y < maze.length
 }
 
+function lowestFScoreNode(scores) {
+  const lowestScore = Math.min(...Object.values(scores))
+  return Object.keys(scores).find(node => scores[node] === lowestScore)
+}
+
 class Node {
-  constructor(maze, x, y) {
-    if (!isInRange(maze, x, y)) {
+  constructor(maze, coordinates) {
+    if (!isInMaze(maze, coordinates.x, coordinates.y)) {
       console.error('this node is not in the maze')
     }
 
     this.maze = maze
-    this.x = x
-    this.y = y
+    this.x = coordinates.x
+    this.y = coordinates.y
   }
 
-  get neighborhood() {
+  neighborhood() {
     const neighborhoodCoordinates = [
       { x: 0, y: -1 }, { x: -1, y: -1 }, { x: -1, y: 0 }, { x: -1, y: 1 },
       { x: 0, y: 1 }, { x: 1, y: 1 }, { x: 1, y: 0 }, { x: 1, y: -1 },
@@ -42,13 +47,30 @@ class Node {
       let x = this.x - coordinates.x
       let y = this.y - coordinates.y
 
-      if (isInRange(maze, x, y)) {
+      if (isInMaze(maze, x, y)) {
         neighborhood.push({ x, y })
       }
     })
 
     return neighborhood
   }
+
+  heuristicCost(coordinates) {
+    return Math.abs(coordinates.x - this.x) + Math.abs(coordinates.y - this.y)
+  }
 }
 
-console.log(new Node(maze, 12, 13).neighborhood)
+function solve(maze, startCoordinates, goalCoordinates) {
+  let start = new Node(maze, startCoordinates)
+
+  let closedSet = []
+  let openSet = [ start ]
+  let cameFrom = {}
+
+  let gScore = { start: 0 }
+  let fScore = { start: start.heuristicCost(goalCoordinates) }
+
+  while (openSet.length > 0) {
+    const current = lowestFScoreNode(fScore)
+  }
+}
